2842e2c383c89a705ae12f6359bef56cc2a0416a
Catalina-S <catalina.sanchez-m@mail.escuelaing.edu.co>
2025-04-04 22:08:24 -0500

    cambio3

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ document.getElementById('MedicamentosForm').addEventListener('submit', function(
     };
   
     // Enviar la solicitud al backend
-    fetch('https://hl7-fhir-ehr.onrender.com/medication-request', {
+    fetch('https://meds-backend-fjhd.onrender.com/medication-request', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
